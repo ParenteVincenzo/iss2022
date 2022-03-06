ae06ffbbfbd4cf5c2912bd3684353ea49b928f7a
Vincenzo Parente <vincenzo.parente@studio.unibo.it>
2022-03-06 17:17:09 +0100

    progetto iniziale

---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
@@ -0,0 +1,11 @@
+package it.unibo.radarSystem22.main;
+
+public class RadarUsageMain {
+	public void doJob() {
+        System.out.println("start");
+	}
+	
+	public static void main(String[] args) {
+        new RadarUsageMain().doJob();
+	}
+}
