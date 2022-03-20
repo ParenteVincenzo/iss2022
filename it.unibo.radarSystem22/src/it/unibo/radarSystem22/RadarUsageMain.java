84c4f5de5efcfd5c154676fd925efa83853a8738
Vincenzo Parente <vincenzo.parente@studio.unibo.it>
2022-03-20 13:58:33 +0100

    riorganizzati i package nel progetto principale

---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/RadarUsageMain.java
@@ -0,0 +1,11 @@
+package it.unibo.radarSystem22;
+
+public class RadarUsageMain {
+	public void doJob() {
+        System.out.println("start");
+	}
+	
+	public static void main(String[] args) {
+        new RadarUsageMain().doJob();
+	}
+}
